36bb51bbd858d0b140a751d3ea3fe1e844bdce34
GkhKaya <gkhkaya00@gmail.com>
2024-06-17 22:26:42 +0300

    Started project

---
+++ OpenAiBot/OpenAiBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenAiBotApp.swift
+//  OpenAiBot
+//
+//  Created by Gokhan Kaya on 17.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct OpenAiBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
